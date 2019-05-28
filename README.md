7f9b78a0bcfcbeb74ba7c05c09854b1535f78298
Diana <dianavcarrara@gmail.com>
2019-05-28 14:02:33 -0600

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# GIPHY
+# GifTastic - A web based Gif fetching machine.
+A Web based Javascript/ HTML5 Gif fetching application that uses the GIPHY API to fetch users choice of GIFs. GIFs have an click to play/ stop feature when clicked.
+
+## Tech Used
+* HTML5
+* CSS
+* JAVASCRIPT
+* CSS
+* Giphy API
+* Ajax
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -32,11 +32,9 @@ $(function() {
       var results = response.data;
 
       for (var i = 0; i < results.length; i++) {
-        
         var rating = results[i].rating;
         var p = $("<p>").text("Rating: " + rating);
-        var animalDiv = $("<div>");
-        animalDiv.addClass("animal-div")
+        var animalDiv = $("<div class='col-md-3'>");
         var animalAnimated = results[i].images.fixed_height.url;
         var animalStatic = results[i].images.fixed_height_still.url;
         var animalPicture = $("<img>");
@@ -45,37 +43,32 @@ $(function() {
         animalPicture.attr("data-still", animalStatic);
         animalPicture.attr("data-state", "still");
         animalPicture.addClass("gif");
-        animalPicture.attr("data-animate",animalAnimated);
+        animalPicture.attr("data-animate", animalAnimated);
         animalDiv.append(p);
-        animalDiv.append(animalPicture)
+        animalDiv.append(animalPicture);
         $("#gifs-appear-here").prepend(animalDiv);
-        
       }
     });
-
-      
-    
   });
 
   $(document).on("click", ".gif", function() {
-      var state = $(this).attr("data-state");
-      console.log("click works" + state)
-      if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-      } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-      }
-    });
-
+    var state = $(this).attr("data-state");
+    console.log("click works" + state);
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
 
   $("#add-animal").on("click", function() {
     event.preventDefault();
-    var addedByUser = $("#animal-input").val().trim();
+    var addedByUser = $("#animal-input")
+      .val()
+      .trim();
     topics.push(addedByUser);
     addTheButtons();
-    
   });
-
 });
